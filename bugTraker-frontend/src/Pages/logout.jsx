e7155a5d600077e78d091035b44073d06d22fd0b
Bhanu Pratap <pratap.b2001@gmail.com>
2026-03-02 22:56:52 +0530

    logout

--- bugTraker-frontend/src/Pages/logout.jsx
+++ bugTraker-frontend/src/Pages/logout.jsx
@@ -3,13 +3,13 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar";
 function Logout({ logout, setUser }) {
-  
+
   const navigate = useNavigate();
   useEffect(() => {
     const dologout = async () => {
       try {
         await logout();
-        setUser("");
+        setUser(null);
         navigate("/login", { replace: "true" });
       } catch (err) {
         console.error("Logout failed", err);
