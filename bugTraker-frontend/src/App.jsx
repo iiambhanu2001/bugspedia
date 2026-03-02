5429acc95b90257e614c7cb5804a5adc63c8ed91
Bhanu Pratap <pratap.b2001@gmail.com>
2026-03-02 22:52:17 +0530

    user commit

--- bugTraker-frontend/src/App.jsx
+++ bugTraker-frontend/src/App.jsx
@@ -66,7 +66,7 @@ function App() {
     if (user) {
       authfrontend();
     }
-  }, [user]);
+  }, []);
 
   async function addbugs(newbug) {
     const res = await fetch(`${import.meta.env.VITE_API_URL}/api/bug/`, {
--- bugTraker-frontend/src/Pages/logout.jsx
+++ bugTraker-frontend/src/Pages/logout.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar";
 function Logout({ logout, setUser }) {
+  
   const navigate = useNavigate();
   useEffect(() => {
     const dologout = async () => {
